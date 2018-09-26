6baf5f81f0df8f76153864af1d5a38842e837c04
jafow <jared.a.fowler@gmail.com>
2018-09-26 00:15:40 -0700

    update tests arg types:
    bytes now

--- tests/test.rs
+++ tests/test.rs
@@ -1,16 +1,16 @@
 extern crate b64rs;
 
 #[test]
-fn test_single_char() {
-    let man: &str = "M";
+fn test_encode_single_word() {
+    let man: &[u8] = b"Man";
     let output = b64rs::encode(man);
 
-    assert_eq!(output, "TQ==");
+    assert_eq!(output.unwrap(), "TWFu");
 }
 
 #[test]
-fn test_multi_word() {
-    let whoa: &str = "hello world";
+fn test_encode_multi_word() {
+    let whoa: &[u8] = b"hello world";
     let out_whao = b64rs::encode(whoa);
 
     assert_eq!(out_whao, "aGVsbG8gd29ybGQ=");
