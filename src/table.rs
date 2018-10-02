f2429ac6f1ce1af87b15bceb168b9e01526f76b5
jafow <jared.a.fowler@gmail.com>
2018-10-01 22:03:47 -0700

    Use inclusive range to build table

--- src/table.rs
+++ src/table.rs
@@ -1,7 +1,7 @@
 pub fn standard() -> Vec<u8> {
-    (b'A'..b'Z')
-        .chain(b'a'..b'z')
-        .chain(0..9)
+    (b'A'..=b'Z')
+        .chain(b'a'..=b'z')
+        .chain(0..=9)
         .chain(vec![b'+', b'/'].into_iter())
         .collect()
 }
