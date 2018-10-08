dced34b2a3d831d40c4be5cc6b278f0a486b5215
jafow <jared.a.fowler@gmail.com>
2018-10-08 00:02:43 -0700

    fix doc example data
    fix shifting

--- src/decode.rs
+++ src/decode.rs
@@ -15,20 +15,43 @@ const BITMASK: BitMask = BitMask {
     be6: 0xfc,
     le2: 0x03,
     le4: 0xf,
-    le56: 0x3,
+    le56: 0x30,
     le6: 0x3f
 };
 
 const SENT: u8 = 255;
 const PAD: u8 = 64;
 
-pub fn decode(s: &[u8]) -> Vec<u8>  {
+/// lookup each char in reverse table
+/// ie [b"T", b"W", b"F", b"u"] => [19, 22, 5, 46] => [77, 67, 110]
+/// then smoosh those ints into 3 bytes
+///
+/// example:
+///     A = [0 0(0 1 0 0 1 1, 0 0 0 1) (0 1 1 0, 0 0 0 0 0 1) (0 1, 1 0 1 0 1 1 1 0)]
+///
+/// bit1 =  |0 0 0 1 0 0 1 1| << 2
+///         |0 1 0 0 1 1 0 0|
+///
+///      +  
+///
+/// bit2    |0 0 0 1 0 1 0 1| & | 0 0 0 1 0 0 0 0 | 
+///         |0 0 0 1 0 0 0 0|
+///                  > > > >
+///         |0 0 0 0 0 0 0 1|
+///
+///      ==
+///
+///         |0 1 0 0 1 1 0 1|
+///
+///
+///
+pub fn decode(s: &[u8]) -> &[u8] {
     let len = s.len();
     let mut output = Vec::new();
 
     for c in s.chunks(4) {
-        output.push((&c[0]) + (&c[1] & BITMASK.le56 >> 4));
-        output.push((&c[1] & BITMASK.le4 << 4) + (&c[2] & BITMASK.le56 >> 4));
+        output.push((&c[0] << 2) + (&c[1] & BITMASK.le56 >> 4));
+        output.push(((&c[1] & BITMASK.be4) << 2) + ((&c[2] & BITMASK.le2) >> 2));
     }
     output
 }
